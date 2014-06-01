1861f3f7cdfcb2bbd058817a1aa2a6806c8b56ad
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2014-06-01 10:34:57 +0200

    fix licence

--- examples/usrp/top_block.py
+++ examples/usrp/top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Wed May 28 18:44:29 2014
+# Generated: Fri May 30 18:05:05 2014
 ##################################################
 
 from PyQt4 import Qt
@@ -50,9 +50,9 @@ class top_block(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 250000
-        self.packet_len = packet_len = 2**16
-        self.decim_fac = decim_fac = 2**8
-        self.wait_to_start = wait_to_start = 0.05
+        self.packet_len = packet_len = 2**18
+        self.decim_fac = decim_fac = 2**10
+        self.wait_to_start = wait_to_start = 0.02
         self.t_measure = t_measure = packet_len/float(samp_rate)
         self.packet_len_red = packet_len_red = packet_len/decim_fac
         self.num_delay_samp = num_delay_samp = 0
@@ -86,16 +86,13 @@ class top_block(gr.top_block, Qt.QWidget):
         self._num_delay_samp_slider.valueChanged.connect(self.set_num_delay_samp)
         self._num_delay_samp_layout.addWidget(self._num_delay_samp_slider)
         self.top_layout.addLayout(self._num_delay_samp_layout)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=decim_fac,
-                taps=None,
-                fractional_bw=None,
-        )
-        self.radar_usrp_echotimer_cc_0 = radar.usrp_echotimer_cc(samp_rate, center_freq, int(num_delay_samp), 'addr=192.168.10.6', '', 'internal', 'none', 'J1', 0.1, wait_to_start, 0, 'addr=192.168.10.4', '', 'mimo', 'mimo', 'J1', 0.1, wait_to_start, 0, "packet_len")
-        (self.radar_usrp_echotimer_cc_0).set_min_output_buffer(131072)
+        self.radar_usrp_echotimer_cc_0 = radar.usrp_echotimer_cc(samp_rate, center_freq, int(num_delay_samp), 'addr=192.168.10.6', '', 'internal', 'none', 'J1', 10, 0.1, wait_to_start, 0, 'addr=192.168.10.4', '', 'mimo', 'mimo', 'J1', 20, 0.1, wait_to_start, 0, "packet_len")
+        (self.radar_usrp_echotimer_cc_0).set_min_output_buffer(524288)
         self.radar_signal_generator_cw_c_0 = radar.signal_generator_cw_c(packet_len, samp_rate, (0, ), 0.5, "packet_len")
-        (self.radar_signal_generator_cw_c_0).set_min_output_buffer(131072)
+        (self.radar_signal_generator_cw_c_0).set_min_output_buffer(524288)
+        self.radar_print_peaks_0 = radar.print_peaks()
+        self.radar_os_cfar_c_0 = radar.os_cfar_c(samp_rate/decim_fac, 15, 0, 0.78, 50, True, "packet_len")
+        (self.radar_os_cfar_c_0).set_min_output_buffer(524288)
         self.qtgui_sink_x_0 = qtgui.sink_c(
         	packet_len/decim_fac, #fftsize
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -112,21 +109,29 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
         
         
+        self.low_pass_filter_0 = filter.fir_filter_ccf(decim_fac, firdes.low_pass(
+        	1, samp_rate, 500, 1000, firdes.WIN_HAMMING, 6.76))
+        (self.low_pass_filter_0).set_min_output_buffer(524288)
         self.blocks_tagged_stream_multiply_length_0 = blocks.tagged_stream_multiply_length(gr.sizeof_gr_complex*1, "packet_len", 1.0/decim_fac)
-        (self.blocks_tagged_stream_multiply_length_0).set_min_output_buffer(131072)
+        (self.blocks_tagged_stream_multiply_length_0).set_min_output_buffer(524288)
         self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
-        (self.blocks_multiply_conjugate_cc_0).set_min_output_buffer(131072)
+        (self.blocks_multiply_conjugate_cc_0).set_min_output_buffer(524288)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_tagged_stream_multiply_length_0, 0))
         self.connect((self.radar_signal_generator_cw_c_0, 0), (self.radar_usrp_echotimer_cc_0, 0))
-        self.connect((self.radar_signal_generator_cw_c_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
         self.connect((self.radar_usrp_echotimer_cc_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
-        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.blocks_tagged_stream_multiply_length_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.radar_signal_generator_cw_c_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_tagged_stream_multiply_length_0, 0))
+        self.connect((self.blocks_tagged_stream_multiply_length_0, 0), (self.radar_os_cfar_c_0, 0))
 
+        ##################################################
+        # Asynch Message Connections
+        ##################################################
+        self.msg_connect(self.radar_os_cfar_c_0, "Msg out", self.radar_print_peaks_0, "Msg in")
 
 # QT sink close method reimplementation
     def closeEvent(self, event):
@@ -139,8 +144,9 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_freq_res(self.samp_rate/float(self.packet_len))
         self.set_t_measure(self.packet_len/float(self.samp_rate))
+        self.set_freq_res(self.samp_rate/float(self.packet_len))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 500, 1000, firdes.WIN_HAMMING, 6.76))
         self.qtgui_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate/self.decim_fac)
 
     def get_packet_len(self):
@@ -148,10 +154,10 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_packet_len(self, packet_len):
         self.packet_len = packet_len
-        self.set_min_output_buffer(self.packet_len*2)
-        self.set_freq_res(self.samp_rate/float(self.packet_len))
         self.set_packet_len_red(self.packet_len/self.decim_fac)
         self.set_t_measure(self.packet_len/float(self.samp_rate))
+        self.set_freq_res(self.samp_rate/float(self.packet_len))
+        self.set_min_output_buffer(self.packet_len*2)
 
     def get_decim_fac(self):
         return self.decim_fac
