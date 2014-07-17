e93dc3029565928c5e0c40d9b1d5fc74ac5912ab
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2014-07-17 12:02:25 +0200

    fix xml crop matrix

--- examples/simulators/top_block.py
+++ examples/simulators/top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Jul 16 20:16:52 2014
+# Generated: Thu Jul 17 12:01:32 2014
 ##################################################
 
 from PyQt4 import Qt
@@ -51,7 +51,8 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.velocity = velocity = 0
+        self.vlen_decim = vlen_decim = 2
+        self.velocity = velocity = 500
         self.transpose_len = transpose_len = 20
         self.sync_word2 = sync_word2 = [0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 0, 1, -1, 1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, 0, 0, 0, 0, 0] 
         self.sync_word1 = sync_word1 = [0., 0., 0., 0., 0., 0., 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., -1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., -1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 1.41421356, 0., 0., 0., 0., 0., 0.]
@@ -74,7 +75,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self._velocity_layout = Qt.QVBoxLayout()
         self._velocity_tool_bar = Qt.QToolBar(self)
         self._velocity_layout.addWidget(self._velocity_tool_bar)
-        self._velocity_tool_bar.addWidget(Qt.QLabel("velocity"+": "))
+        self._velocity_tool_bar.addWidget(Qt.QLabel("Velocity"+": "))
         class qwt_counter_pyslot(Qwt.QwtCounter):
             def __init__(self, parent=None):
                 Qwt.QwtCounter.__init__(self, parent)
@@ -93,7 +94,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self._velocity_slider.setMinimumWidth(200)
         self._velocity_slider.valueChanged.connect(self.set_velocity)
         self._velocity_layout.addWidget(self._velocity_slider)
-        self.top_layout.addLayout(self._velocity_layout)
+        self.top_grid_layout.addLayout(self._velocity_layout, 0,1)
         self._Range_layout = Qt.QVBoxLayout()
         self._Range_tool_bar = Qt.QToolBar(self)
         self._Range_layout.addWidget(self._Range_tool_bar)
@@ -105,23 +106,24 @@ class top_block(gr.top_block, Qt.QWidget):
             def setValue(self, value):
                 super(Qwt.QwtCounter, self).setValue(value)
         self._Range_counter = qwt_counter_pyslot()
-        self._Range_counter.setRange(0.1, 10000, 1)
+        self._Range_counter.setRange(0.1, 2000, 1)
         self._Range_counter.setNumButtons(2)
         self._Range_counter.setValue(self.Range)
         self._Range_tool_bar.addWidget(self._Range_counter)
         self._Range_counter.valueChanged.connect(self.set_Range)
         self._Range_slider = Qwt.QwtSlider(None, Qt.Qt.Horizontal, Qwt.QwtSlider.BottomScale, Qwt.QwtSlider.BgSlot)
-        self._Range_slider.setRange(0.1, 10000, 1)
+        self._Range_slider.setRange(0.1, 2000, 1)
         self._Range_slider.setValue(self.Range)
         self._Range_slider.setMinimumWidth(200)
         self._Range_slider.valueChanged.connect(self.set_Range)
         self._Range_layout.addWidget(self._Range_slider)
-        self.top_layout.addLayout(self._Range_layout)
+        self.top_grid_layout.addLayout(self._Range_layout, 0,0)
         self.radar_transpose_matrix_vcvc_0_0 = radar.transpose_matrix_vcvc(transpose_len, fft_len, "packet_len")
         self.radar_transpose_matrix_vcvc_0 = radar.transpose_matrix_vcvc(fft_len, transpose_len, "packet_len")
         self.radar_static_target_simulator_cc_0 = radar.static_target_simulator_cc((Range, ), (velocity, ), (1e25, ), (0, ), samp_rate, center_freq, -10, True, True, "packet_len")
-        self.radar_qtgui_spectrogram_plot_0 = radar.qtgui_spectrogram_plot(fft_len, 500, 'Range', 'Velocity', 'OFDM Radar', (0,10), (0,10), (-15,-8), True, "packet_len")
+        self.radar_qtgui_spectrogram_plot_0 = radar.qtgui_spectrogram_plot(fft_len/vlen_decim, 500, 'Range', 'Velocity', 'OFDM Radar', (0,10), (0,10), (-15,-5), True, "packet_len")
         self.radar_ofdm_cyclic_prefix_remover_cvc_0 = radar.ofdm_cyclic_prefix_remover_cvc(fft_len, fft_len/4, "packet_len")
+        self.radar_crop_matrix_vcvc_0 = radar.crop_matrix_vcvc(fft_len, (fft_len/2,fft_len), (0,transpose_len), "packet_len")
         self.fft_vxx_0_1_0 = fft.fft_vcc(transpose_len, False, (()), True, 1)
         self.fft_vxx_0_1 = fft.fft_vcc(fft_len, True, (()), True, 1)
         self.fft_vxx_0_0 = fft.fft_vcc(fft_len, True, (()), True, 1)
@@ -132,10 +134,9 @@ class top_block(gr.top_block, Qt.QWidget):
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
         self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, packet_len, length_tag_key)
         self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(8, payload_mod.bits_per_symbol(), length_tag_key, False)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*fft_len)
-        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(1, fft_len, 0)
+        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(1, fft_len/vlen_decim, 0)
         self.blocks_divide_xx_0 = blocks.divide_cc(fft_len)
-        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(fft_len)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(fft_len/vlen_decim)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 255, 1000)), True)
         self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.1, 0)
@@ -158,13 +159,13 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.fft_vxx_0, 0), (self.digital_ofdm_cyclic_prefixer_0, 0))
         self.connect((self.fft_vxx_0_0, 0), (self.blocks_divide_xx_0, 1))
         self.connect((self.digital_ofdm_carrier_allocator_cvc_0, 0), (self.blocks_divide_xx_0, 0))
-        self.connect((self.radar_transpose_matrix_vcvc_0_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
-        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_nlog10_ff_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.radar_qtgui_spectrogram_plot_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.blocks_null_sink_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.radar_ofdm_cyclic_prefix_remover_cvc_0, 0))
         self.connect((self.radar_static_target_simulator_cc_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.radar_transpose_matrix_vcvc_0_0, 0), (self.radar_crop_matrix_vcvc_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_nlog10_ff_0, 0))
+        self.connect((self.blocks_nlog10_ff_0, 0), (self.radar_qtgui_spectrogram_plot_0, 0))
+        self.connect((self.radar_crop_matrix_vcvc_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
 
 
     def closeEvent(self, event):
@@ -172,14 +173,20 @@ class top_block(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+    def get_vlen_decim(self):
+        return self.vlen_decim
+
+    def set_vlen_decim(self, vlen_decim):
+        self.vlen_decim = vlen_decim
+
     def get_velocity(self):
         return self.velocity
 
     def set_velocity(self, velocity):
         self.velocity = velocity
-        self.radar_static_target_simulator_cc_0.setup_targets((self.Range, ), (self.velocity, ), (1e25, ), (0, ), self.samp_rate, self.center_freq, -10, True, True)
         Qt.QMetaObject.invokeMethod(self._velocity_counter, "setValue", Qt.Q_ARG("double", self.velocity))
         Qt.QMetaObject.invokeMethod(self._velocity_slider, "setValue", Qt.Q_ARG("double", self.velocity))
+        self.radar_static_target_simulator_cc_0.setup_targets((self.Range, ), (self.velocity, ), (1e25, ), (0, ), self.samp_rate, self.center_freq, -10, True, True)
 
     def get_transpose_len(self):
         return self.transpose_len
@@ -273,9 +280,9 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_Range(self, Range):
         self.Range = Range
-        self.radar_static_target_simulator_cc_0.setup_targets((self.Range, ), (self.velocity, ), (1e25, ), (0, ), self.samp_rate, self.center_freq, -10, True, True)
         Qt.QMetaObject.invokeMethod(self._Range_counter, "setValue", Qt.Q_ARG("double", self.Range))
         Qt.QMetaObject.invokeMethod(self._Range_slider, "setValue", Qt.Q_ARG("double", self.Range))
+        self.radar_static_target_simulator_cc_0.setup_targets((self.Range, ), (self.velocity, ), (1e25, ), (0, ), self.samp_rate, self.center_freq, -10, True, True)
 
 if __name__ == '__main__':
     import ctypes
