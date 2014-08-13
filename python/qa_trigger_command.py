8d562039ff02ad6213c9d36fa0ea1ccace339947
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2014-08-13 16:03:38 +0200

    add testcase trigger command

--- python/qa_trigger_command.py
+++ python/qa_trigger_command.py
@@ -22,20 +22,38 @@
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 import radar_swig as radar
+import pmt
+from time import sleep
 
 class qa_trigger_command (gr_unittest.TestCase):
 
-    def setUp (self):
-        self.tb = gr.top_block ()
+	def setUp (self):
+		self.tb = gr.top_block ()
 
-    def tearDown (self):
-        self.tb = None
+	def tearDown (self):
+		self.tb = None
 
-    def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+	def test_001_t (self):
+		# set up fg
+		Range = (10, 20, 30)
+		power = (15, 30, 45)
+
+		pmt_range = pmt.list2(pmt.string_to_symbol('range'),pmt.init_f32vector(len(Range),Range))
+		pmt_power = pmt.list2(pmt.string_to_symbol('power'),pmt.init_f32vector(len(power),power))
+		pmt_misc = pmt.list2(pmt.string_to_symbol('misc'),pmt.init_f32vector(3,(1,2,3)))
+		pmt_in = pmt.list3(pmt_misc,pmt_range,pmt_power)
+
+		src = blocks.message_strobe(pmt_in, 300)
+		test = radar.trigger_command("echo test_command",("Range","power"),(5, 10),(35, 50))
+		
+		self.tb.msg_connect(src,"strobe",test,"Msg in")
+		
+		self.tb.start()
+		sleep(0.5)
+		self.tb.stop()
+		self.tb.wait()
+		# check data
 
 
 if __name__ == '__main__':
-    gr_unittest.run(qa_trigger_command, "qa_trigger_command.xml")
+	gr_unittest.run(qa_trigger_command)#, "qa_trigger_command.xml")
