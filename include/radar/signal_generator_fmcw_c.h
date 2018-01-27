8ecc6cc22f2d795873880a639897c4a1e5ebf5e9
Martin Braun <martin.braun@ettus.com>
2018-01-27 10:31:12 +0100

    Refactor siggen FMCW
    - No functional changes
    - Attributes are properly const and/or private
    - Updated some comments

--- include/radar/signal_generator_fmcw_c.h
+++ include/radar/signal_generator_fmcw_c.h
@@ -1,23 +1,23 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2014 Communications Engineering Lab, KIT.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
- 
+
 #ifndef INCLUDED_RADAR_SIGNAL_GENERATOR_FMCW_C_H
 #define INCLUDED_RADAR_SIGNAL_GENERATOR_FMCW_C_H
 
@@ -27,22 +27,22 @@
 namespace gr {
   namespace radar {
 
-    /*!
-     * \brief This block generates a signal for FMCW radar in baseband. The modulation is structured in a CW part with constant frequency and a following up- and down-chirp.
-     * The chirp goes from CW frequency to CW frequency plus sweep frequency and back to the CW frequency. The CW part is first, up- and down-chirp is following.
-     * The packet length for subsequent tagged streams is calculated by the sum of the number of samples of the single modulations parts.
-     * 
-     * \param samp_rate Signal sample rate
-     * \param samp_up Number samples of up-chirp part
-     * \param samp_down Number samples of down-chirp part
-     * \param samp_cw Number samples of CW part
-     * \param freq_cw CW signal frequency in baseband
-     * \param freq_sweep Sweep frequency of up- and down-chirp
-     * \param amplitude Signal amplitude
-     * \param len_key Packet length key for tagged stream
-     * 
-     * \ingroup radar
+    /*! Generates a signal for FMCW radar in baseband.
      *
+     * The generated signal consists of three parts, in this order:
+     *
+     * 1. CW part with constant frequency
+     * 2. Up-chirp
+     * 3. Down-chirp
+     *
+     * The up-chirp goes from CW frequency to CW frequency plus sweep frequency
+     * and the down-chirp goes in the opposite direction. All of these parts
+     * can be disabled by setting the corresponding length to zero.
+     *
+     * The packet length for subsequent tagged streams is calculated by the sum
+     * of the number of samples of the single modulations parts.
+     *
+     * \ingroup radar
      */
     class RADAR_API signal_generator_fmcw_c : virtual public gr::sync_block
     {
@@ -50,18 +50,28 @@ namespace gr {
       typedef boost::shared_ptr<signal_generator_fmcw_c> sptr;
 
       /*!
-       * \brief Return a shared_ptr to a new instance of radar::signal_generator_fmcw_c.
-       *
-       * To avoid accidental use of raw pointers, radar::signal_generator_fmcw_c's
-       * constructor is in a private implementation
-       * class. radar::signal_generator_fmcw_c::make is the public interface for
-       * creating new instances.
+       * \param samp_rate Signal sample rate (samples per second)
+       * \param samp_up Number samples of up-chirp part
+       * \param samp_down Number samples of down-chirp part
+       * \param samp_cw Number samples of CW part
+       * \param freq_cw CW signal frequency in baseband
+       * \param freq_sweep Sweep frequency of up- and down-chirp
+       * \param amplitude Signal amplitude
+       * \param len_key Packet length key for tagged stream
        */
-      static sptr make(int samp_rate, int samp_up, int samp_down, int samp_cw, float freq_cw, float freq_sweep, float amplitude, const std::string& len_key="packet_len");
+      static sptr make(
+        const int samp_rate,
+        const int samp_up,
+        const int samp_down,
+        const int samp_cw,
+        const float freq_cw,
+        const float freq_sweep,
+        const float amplitude,
+        const std::string& len_key="packet_len"
+      );
     };
 
   } // namespace radar
 } // namespace gr
 
 #endif /* INCLUDED_RADAR_SIGNAL_GENERATOR_FMCW_C_H */
-
