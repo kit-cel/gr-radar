d9ac7a21648ad85eba4dee1a07d15c17c7896c7a
Nicolas Cuervo <cuervonicolas@gmail.com>
2019-11-19 21:08:26 +0100

    Python: fix QA tests for python3

--- python/qa_ofdm_cyclic_prefix_remover_cvc.py
+++ python/qa_ofdm_cyclic_prefix_remover_cvc.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2014 Communications Engineering Lab, KIT.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks, digital
@@ -36,9 +36,9 @@ class qa_ofdm_cyclic_prefix_remover_cvc (gr_unittest.TestCase):
         test_len = 1000
         fft_len = 100;
         cp_len = fft_len/4
-        
-        in_data = range(0,fft_len)*(test_len/fft_len);
-        
+
+        in_data = list(range(0,fft_len))*(test_len//fft_len);
+
         src = blocks.vector_source_c(in_data)
         s2v = blocks.stream_to_vector(8,fft_len)
         s2ts = blocks.stream_to_tagged_stream(8,fft_len,test_len/fft_len,'packet_len')
@@ -46,18 +46,18 @@ class qa_ofdm_cyclic_prefix_remover_cvc (gr_unittest.TestCase):
         cp_remove = radar.ofdm_cyclic_prefix_remover_cvc(fft_len,cp_len,'packet_len')
         v2s = blocks.vector_to_stream(8,fft_len)
         snk = blocks.vector_sink_c()
-        
+
         self.tb.connect(src,s2v,s2ts,cp_add,cp_remove,v2s,snk)
         self.tb.run ()
-        
+
         # check data
         out_data = snk.data()
         out_data_real = [0]*len(out_data)
         for k in range(len(out_data)):
-			out_data_real[k] = out_data[k].real
-        
+            out_data_real[k] = out_data[k].real
+
         for k in range(len(in_data)):
-			self.assertEqual(in_data[k],out_data_real[k])
+            self.assertEqual(in_data[k],out_data_real[k])
 
 
 if __name__ == '__main__':
--- python/qa_split_fsk_cc.py
+++ python/qa_split_fsk_cc.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2014 Communications Engineering Lab, KIT.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
@@ -33,53 +33,52 @@ class qa_split_fsk_cc (gr_unittest.TestCase):
 
     def test_001_t (self):
         # set up fg
-		test_len = 1200
-		
-		samp_rate = 4000
-		samp_per_freq = 2
-		blocks_per_tag = 100
-		freq_low = 0
-		freq_high = 200
-		amplitude = 1
-		
-		samp_discard = 1
-		
-		src = radar.signal_generator_fsk_c(samp_rate, samp_per_freq, blocks_per_tag, freq_low, freq_high, amplitude)
-		head = blocks.head(8,test_len)
-		split = radar.split_fsk_cc(samp_per_freq,samp_discard)
-		snk0 = blocks.vector_sink_c()
-		snk1 = blocks.vector_sink_c()
-		snk = blocks.vector_sink_c()
-		
-		self.tb.connect(src,head,split)
-		self.tb.connect((split,0),snk0)
-		self.tb.connect((split,1),snk1)
-		self.tb.connect(head,snk)
-		self.tb.run ()
-		
-		# check correct length of data
-		nblock = test_len/2/samp_per_freq
-		self.assertEqual(len(snk0.data()),nblock*(samp_per_freq-samp_discard))
-		self.assertEqual(len(snk1.data()),nblock*(samp_per_freq-samp_discard))
-		
-		# split data self
-		data = snk.data()
-		data0 = []
-		data1 = []
-		k=0
-		while k<len(data):
-			k = k+samp_discard
-			data0.append(data[k])
-			k = k+1
-			k = k+samp_discard
-			data1.append(data[k])
-			k = k+1
-		
-		# check if data0/1 is same as split from block
-		self.assertComplexTuplesAlmostEqual(data0,snk0.data(),10)
-		self.assertComplexTuplesAlmostEqual(data1,snk1.data(),10)
-			
-		
+        test_len = 1200
+        samp_rate = 4000
+        samp_per_freq = 2
+        blocks_per_tag = 100
+        freq_low = 0
+        freq_high = 200
+        amplitude = 1
+
+        samp_discard = 1
+
+        src = radar.signal_generator_fsk_c(samp_rate, samp_per_freq, blocks_per_tag, freq_low, freq_high, amplitude)
+        head = blocks.head(8,test_len)
+        split = radar.split_fsk_cc(samp_per_freq,samp_discard)
+        snk0 = blocks.vector_sink_c()
+        snk1 = blocks.vector_sink_c()
+        snk = blocks.vector_sink_c()
+
+        self.tb.connect(src,head,split)
+        self.tb.connect((split,0),snk0)
+        self.tb.connect((split,1),snk1)
+        self.tb.connect(head,snk)
+        self.tb.run ()
+
+        # check correct length of data
+        nblock = test_len/2/samp_per_freq
+        self.assertEqual(len(snk0.data()),nblock*(samp_per_freq-samp_discard))
+        self.assertEqual(len(snk1.data()),nblock*(samp_per_freq-samp_discard))
+
+        # split data self
+        data = snk.data()
+        data0 = []
+        data1 = []
+        k=0
+        while k<len(data):
+            k = k+samp_discard
+            data0.append(data[k])
+            k = k+1
+            k = k+samp_discard
+            data1.append(data[k])
+            k = k+1
+
+        # check if data0/1 is same as split from block
+        self.assertComplexTuplesAlmostEqual(data0,snk0.data(),10)
+        self.assertComplexTuplesAlmostEqual(data1,snk1.data(),10)
+
+
 
 if __name__ == '__main__':
     gr_unittest.run(qa_split_fsk_cc, "qa_split_fsk_cc.xml")
